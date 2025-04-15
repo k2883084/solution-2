39863b194ca328cd273592262bd916aa2ae87563
k2883084 <k2883084@gmail.com>
2025-04-15 01:43:09 -0700

    Add files via upload

---
+++ solution2--kirti/solution2--kirti.js
@@ -0,0 +1,62 @@
+//Q.1 printing daily tasks
+//scenerio: you have a list of daily task
+
+//for(let tasknumber = 1; tasknumber <= 5; tasknumber++){
+  //  console.log("Task", tasknumber);
+//}
+
+//Q.2 counting sheep
+//scenerio: you're trying to fall asleepmand decide to count sheep.
+//for(let sheepnumber = 1; sheepnumber <= 3; sheepnumber++){
+  //  console.log("sheep", sheepnumber);
+//}
+//Q.3 making a stack of blocks
+//scenerio: you're stacking blocks.
+//for(let levelnumber = 1; levelnumber <= 4; levelnumber++){
+  //    console.log("block placed on level", levelnumber);
+//}
+
+//Q.4 printing a countdown
+//scenerio: you're launching a rocke, and you need to countdown 3 to1.
+ 
+//for(let countdownnumber = 3; countdownnumber >= 1; countdownnumber--){
+  //console.log( countdownnumber);
+//}
+
+
+//Q.5 collecting coins
+//scenerio: you're playing a game, and you collect 2 coins per level for 3 levels. use a loop to calculate and print the total
+//number of coins collected
+//let totalCoins = 0;
+//let coinsPerLevels = 2;
+//let levels = 3;
+//for(let i = 1; i <= levels; i++){
+  //totalCoins +=coinsPerLevels;
+//}
+//console.log("total Coins Collected:" + totalCoins);
+
+//Q.6 printing star pattern
+//you want to print three lines of stars, each conating two stars. use a loop to print the line of stars.
+//for(i = 1; i <= 3; i++){
+ //console.log("**");
+//}
+
+
+
+//Q.7 simple time tracking
+//scenerio: you want to track the passing of 4 seconds.
+//for(let secondnumber = 1; secondnumber <= 4; secondnumber++){
+//console.log( "second", secondnumber);
+//}
+
+//Q.8 filling buckets
+//scenerio: you need to fill buckets with water until you've filled 3 buckets. use a loop to print "bucket[bucket number]filled!" for each bucket.
+//for(let bucketnumber = 1; bucketnumber <= 3; bucketnumber++){
+  //console.log("bucket",bucketnumber);
+//}
+
+//Q.9 calculating average excluding extremes
+//scenerio: you have a series of test scores. you need to calculate the average score, but you want
+//to exclude the highest and lowest scores. use a loop to find the highest and lowest scores, then calculate the average 
+// of the remaining scores.
+
